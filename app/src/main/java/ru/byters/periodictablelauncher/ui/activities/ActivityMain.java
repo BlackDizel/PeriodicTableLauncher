beb1cd801ee07c20df541beebd587149f3ca0615
BlackDizel <blackdizel@gmail.com>
2016-03-22 15:58:19 +0300

    add background support

--- app/src/main/java/ru/byters/periodictablelauncher/ui/activities/ActivityMain.java
+++ app/src/main/java/ru/byters/periodictablelauncher/ui/activities/ActivityMain.java
@@ -1,19 +1,25 @@
 package ru.byters.periodictablelauncher.ui.activities;
 
+import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Matrix;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 
 import ru.byters.periodictablelauncher.R;
 import ru.byters.periodictablelauncher.ui.adapters.ItemsAdapter;
 
 public class ActivityMain extends AppCompatActivity {
     private RecyclerView rvView;
+    private ImageView imageView;
+    private int imageWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +27,42 @@ public class ActivityMain extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
         rvView = (RecyclerView) findViewById(R.id.rvItems);
         rvView.setAdapter(new ItemsAdapter(this));
         rvView.addItemDecoration(new AppItemDecoration(this));
         setLayoutManager();
+        setBackground();
+    }
 
-/*        WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
-        Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+    private void setBackground() {
+        Drawable wallpaperDrawable = WallpaperManager.getInstance(this).getDrawable();
+        if (wallpaperDrawable != null) {
+            imageWidth = wallpaperDrawable.getIntrinsicWidth();
+            imageView.setImageDrawable(wallpaperDrawable);
 
-        View rootView = findViewById(R.id.rootView);
-        if (wallpaperDrawable != null && rootView != null)
-            rootView.setBackground(wallpaperDrawable);*/
+            rvView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                    super.onScrollStateChanged(recyclerView, newState);
+                }
+
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+
+                    int imageContainerWidth = imageView.getWidth();
+                    if (imageWidth > imageContainerWidth) {
+                        float ratio = rvView.computeHorizontalScrollOffset() / (float) rvView.computeHorizontalScrollRange();
+                        int pos = -(int) ((imageWidth - imageView.getWidth()) * ratio);
+                        Matrix m = new Matrix();
+                        m.reset();
+                        m.postTranslate(pos, 0);
+                        imageView.setImageMatrix(m);
+                    }
+                }
+            });
+        }
     }
 
     @Override
