4ce6da88acddc8cfe54b28a69987e02483883f24
blackdizel <blackdizel@gmail.com>
2017-12-15 16:00:09 +0300

    move wallpaper image to main activity

--- app/src/main/java/org/byters/periodictablelauncher/ui/activities/ActivityMain.java
+++ app/src/main/java/org/byters/periodictablelauncher/ui/activities/ActivityMain.java
@@ -1,22 +1,46 @@
 package org.byters.periodictablelauncher.ui.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 
 import org.byters.periodictablelauncher.R;
 import org.byters.periodictablelauncher.controllers.ControllerItems;
 import org.byters.periodictablelauncher.controllers.ControllerPreference;
+import org.byters.periodictablelauncher.controllers.ControllerWallpaper;
+import org.byters.periodictablelauncher.controllers.Core;
+import org.byters.periodictablelauncher.controllers.ListenerWallpaperChange;
 import org.byters.periodictablelauncher.ui.NavigationHelper;
 import org.byters.periodictablelauncher.ui.fragments.FragmentIcons;
 import org.byters.periodictablelauncher.ui.fragments.FragmentItemEdit;
 import org.byters.periodictablelauncher.ui.fragments.FragmentItemInfo;
+import org.byters.periodictablelauncher.ui.utils.WallpaperViewHelper;
+import org.byters.periodictablelauncher.view.presenter.IPresenterWallpaper;
+import org.byters.periodictablelauncher.view.presenter.callback.PresenterWallpaperCallback;
 
-public class ActivityMain extends ActivityBase {
+import java.lang.ref.WeakReference;
+
+public class ActivityMain extends ActivityBase
+        implements ListenerWallpaperChange {
+
+    private ImageView imageView;
+    private WallpaperViewHelper wallpaperViewHelper;
+    private WeakReference<IPresenterWallpaper> refPresenterWallpaper;
+    private CallbackWallpaperPresenter callbackWallpaperPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        wallpaperViewHelper = new WallpaperViewHelper();
+
+        refPresenterWallpaper = new WeakReference<>(Core.getInstance().getInjector().getPresenterWallpaper());
+        callbackWallpaperPresenter = new CallbackWallpaperPresenter();
+
         setFragmentGrid();
     }
 
@@ -37,10 +61,25 @@ public class ActivityMain extends ActivityBase {
     }
 
     @Override
+    public void onWallpaperChange() {
+        wallpaperViewHelper.setWallpaper(imageView);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        wallpaperViewHelper.setWallpaper(imageView);
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         NavigationHelper.getInstance().addListener(this);
         ControllerItems.getInstance().updateData();
+
+        ControllerWallpaper.getInstance().addListener(this);
+        wallpaperViewHelper.setWallpaper(imageView);
+        refPresenterWallpaper.get().setCallback(callbackWallpaperPresenter);
     }
 
     @Override
@@ -49,6 +88,7 @@ public class ActivityMain extends ActivityBase {
         NavigationHelper.getInstance().removeListener(this);
         ControllerItems.getInstance().storeData();
         ControllerPreference.getInstance().storeData();
+        ControllerWallpaper.getInstance().removeListener(this);
     }
 
     @Override
@@ -68,5 +108,12 @@ public class ActivityMain extends ActivityBase {
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                 .replace(R.id.flContent, FragmentItemEdit.getFragment())
                 .commit();
+    }
+
+    private class CallbackWallpaperPresenter implements PresenterWallpaperCallback {
+        @Override
+        public void updateWallpaper(RecyclerView recyclerView) {
+            wallpaperViewHelper.updateWallpaperPosition(imageView, recyclerView);
+        }
     }
 }
--- app/src/main/java/org/byters/periodictablelauncher/ui/fragments/FragmentIcons.java
+++ app/src/main/java/org/byters/periodictablelauncher/ui/fragments/FragmentIcons.java
@@ -1,9 +1,6 @@
 package org.byters.periodictablelauncher.ui.fragments;
 
-import android.app.WallpaperManager;
 import android.content.res.Configuration;
-import android.graphics.Matrix;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,23 +9,24 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import org.byters.periodictablelauncher.R;
 import org.byters.periodictablelauncher.controllers.ControllerItems;
 import org.byters.periodictablelauncher.controllers.ControllerPreference;
-import org.byters.periodictablelauncher.controllers.ControllerWallpaper;
+import org.byters.periodictablelauncher.controllers.Core;
 import org.byters.periodictablelauncher.controllers.ListenerAppsUpdate;
-import org.byters.periodictablelauncher.controllers.ListenerWallpaperChange;
 import org.byters.periodictablelauncher.models.ModelPreference;
 import org.byters.periodictablelauncher.ui.adapters.ItemsAdapter;
+import org.byters.periodictablelauncher.view.presenter.IPresenterWallpaper;
+
+import java.lang.ref.WeakReference;
 
 public class FragmentIcons extends FragmentBase
-        implements ListenerAppsUpdate, ListenerWallpaperChange {
+        implements ListenerAppsUpdate {
 
     private RecyclerView rvView;
-    private ImageView imageView;
     private RecyclerViewScrollListener scrollListener;
+    private WeakReference<IPresenterWallpaper> refPresenterWallpaper;
 
     public static Fragment getFragment() {
         return new FragmentIcons();
@@ -38,15 +36,12 @@ public class FragmentIcons extends FragmentBase
     public void onStart() {
         super.onStart();
         ControllerItems.getInstance().addListener(this);
-        ControllerWallpaper.getInstance().addListener(this);
-        setBackground(getView());
     }
 
     @Override
     public void onStop() {
         super.onStop();
         ControllerItems.getInstance().removeListener(this);
-        ControllerWallpaper.getInstance().removeListener(this);
     }
 
     @Override
@@ -59,6 +54,8 @@ public class FragmentIcons extends FragmentBase
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         scrollListener = new RecyclerViewScrollListener();
+
+        refPresenterWallpaper = new WeakReference<>(Core.getInstance().getInjector().getPresenterWallpaper());
     }
 
     @Nullable
@@ -66,7 +63,6 @@ public class FragmentIcons extends FragmentBase
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_icons, container, false);
         setGrid(v);
-        setBackground(v);
         return v;
     }
 
@@ -77,18 +73,9 @@ public class FragmentIcons extends FragmentBase
         rvView.addOnScrollListener(scrollListener);
     }
 
-    private void setBackground(View v) {
-        imageView = (ImageView) v.findViewById(R.id.imageView);
-        Drawable wallpaperDrawable = WallpaperManager.getInstance(getContext()).getDrawable();
-        if (wallpaperDrawable == null) return;
-        imageView.setImageDrawable(wallpaperDrawable);
-        updateWallpaperPosition(new Matrix());
-    }
-
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        setBackground(getView());
         setLayoutManager();
     }
 
@@ -103,47 +90,13 @@ public class FragmentIcons extends FragmentBase
                 , false));
     }
 
-    @Override
-    public void onWallpaperChange() {
-        if (getView() == null) return;
-        setBackground(getView());
-    }
-
-    private void updateWallpaperPosition(Matrix m) {
-        if (imageView.getDrawable() == null) return;
-        m.reset();
-
-        float scale = imageView.getHeight() / (float) imageView.getDrawable().getIntrinsicHeight();
-        m.postScale(scale, scale);
-
-        int imageContainerWidth = imageView.getWidth();
-        float imageWidth = imageView.getDrawable().getIntrinsicWidth() * scale;
-        float xScale = imageContainerWidth / imageWidth;
-        if (xScale > 1)
-            m.postScale(xScale, xScale);
-
-        if (xScale < 1) {
-            float ratio = rvView.computeHorizontalScrollOffset() / (float) rvView.computeHorizontalScrollRange();
-            int pos = -(int) ((imageWidth - imageView.getWidth()) * ratio);
-            m.postTranslate(pos, 0);
-        }
-
-        imageView.setImageMatrix(m);
-    }
-
     private class RecyclerViewScrollListener extends RecyclerView.OnScrollListener {
-
-        private final Matrix m;
-
-        RecyclerViewScrollListener() {
-            m = new Matrix();
-        }
 
         @Override
         public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
             super.onScrolled(recyclerView, dx, dy);
 
-            updateWallpaperPosition(m);
+            refPresenterWallpaper.get().updateWallpaperPosition(rvView);
         }
     }
 }
